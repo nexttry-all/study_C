ce8ca8c281d91be8ab729feb1b4cda817f9ef370
LJL <hello@LJL.com>
2025-03-11 14:22:39 +0800

    添加项目文件。

---
+++ 染色排序/源.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void test1() {
+	static int count = 0;
+	count++;
+	double a=static_cast<double>(count)+0.77;
+	cout << count << "\n";
+	cout << a;
+}
+
+
+int main() {
+
+	test1();
+	return 0;
+}
